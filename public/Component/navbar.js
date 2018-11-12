b4cf3672f94411c7e84b45932c1ff6f83cdeb4f3
allan <raicas@hotmail.fr>
2018-11-12 15:11:27 +0100

    Réorganisation de la navbar

--- public/Component/navbar.js
+++ public/Component/navbar.js
@@ -17,14 +17,20 @@ const navbar  = Vue.component('navbar-app', {
             <li class="nav-item">
               <router-link class="nav-link" to="/Login">Connexion</router-link>
             </li>
-            <li class="nav-item">
-            <a class="nav-link" v-on:click="$emit('enable-dark-mode',true)">    <i class="far fa-moon"></i></a>
-          </li>
+           
           </ul>
-          <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          
+        <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+        <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+        <a class="nav-link" v-on:click="$emit('enable-dark-mode',true)">    <i class="far fa-moon"></i></a>
+      </li>
+        <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
         </form>
+        </ul>
+    </div>
         </div>
       </nav>
       `
