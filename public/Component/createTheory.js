dfb2b576ce6fffdfe7149bcc0e277d451a703763
kawadjou <amekoujc@gmail.com>
2018-11-12 15:33:46 +0100

    maj

--- public/Component/createTheory.js
+++ public/Component/createTheory.js
@@ -1,11 +1,28 @@
 Vue.component('create-theory', {
     props: ['theory'],
     template: `
-    <div class="createTheory">
-    <h1><b>{{ theory.titre }}</b></h1> 
-    <img :src="theory.urlImage" alt="Avatar" >
-      <p>{{ theory.description }}</p> 
-      </div>
+    <div class="carre1">
+    <form style="margin-top: 10%">
+            <p >
+                Title :
+            </p>
+            <p>
+                <input type="text" id="titre" style="width: 200px; height: 30px;" />
+            </p>
+            <p >
+                Description :
+            </p>
+            <p>
+                <input type="text" id="descritpion" class="box"/>
+            </p>
+            <p >
+            Picture :
+            </p>
+            <p>
+                <input type="text" id="img" class="box"/><input type="submit" id="submit" value="Browse"  style="border-style: solid; border-radius: 5px"/>
+            </p> 
+        </form>
+        </div>
       `
   })
 
